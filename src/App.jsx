cdbd56d06dcd09d0b7fdd3335a1ab7601743a420
hackerank <me@hackerank.com>
2025-03-14 20:50:41 +0530

    Added React router dom and navbar and footer inside  body component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
