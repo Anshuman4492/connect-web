2f2c8e115d9b34ff2b0686bdf35108053bd4a4da
hackerank <me@hackerank.com>
2025-03-14 23:24:23 +0530

    Setup Redux Store + Login redirected to feed after login

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Body from "./components/Body";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import appStore from "./utils/appStore";
+import { Provider } from "react-redux";
+import Feed from "./components/Feed";
 
 function App() {
   return (
-    <>
+    <Provider store={appStore}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Body />}>
+            <Route path="/" element={<Feed />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </Provider>
   );
 }
 
